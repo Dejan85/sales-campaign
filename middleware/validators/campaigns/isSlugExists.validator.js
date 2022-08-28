d89a439fd88ea6f8f7e01aa29c553a0c1912db2d
Dejan Markovic <dejan.markovic@cinnamon.agency>
2022-08-28 19:42:12 +0200

    fix campaign validator

--- middleware/validators/campaigns/isSlugExists.validator.js
+++ middleware/validators/campaigns/isSlugExists.validator.js
@@ -1,27 +1,16 @@
 const { tryCatch } = require("../../../utils");
 
 const {
-  campaignsQueries: { getAll },
+  campaignsQueries: { getAll, getBySlug },
 } = require("../../../queries/campaigns");
 
 exports.isSlugExists = tryCatch(async (req) => {
-  const campaign = await getAll();
+  const campaign = await getBySlug(req.body.slug);
 
   if (!!campaign.length) {
-    const testForDuplicate = campaign.some(
-      (campaign) => campaign.slug === req.body.slug
-    );
-
-    if (testForDuplicate) {
-      throw {
-        message: `Slug ${req.body.slug} already exist`,
-        statusCode: 400,
-      };
-    }
-  } else {
     throw {
-      message: `There are no any campaigns`,
-      statusCode: 404,
+      message: `Slug ${req.body.slug} already exist`,
+      statusCode: 400,
     };
   }
 });
