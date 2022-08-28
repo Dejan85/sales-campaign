e80587c744851374fab1fd9346dae383255ee5d7
Dejan Markovic <dejan.markovic@cinnamon.agency>
2022-08-28 19:28:54 +0200

    add production host to url

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 dotenv.config();
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://sales-campaign-fe.vercel.app"],
     credentials: true,
   })
 );
