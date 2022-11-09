c7524445aef7b2b64ca3f04141fae8140de13cce
Dejan Markovic <dejan.markovic@cinnamon.agency>
2022-11-09 20:30:32 +0100

    add https

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(
       "http://127.0.0.1:5500",
       "http://localhost:1234",
       "http://specijalna-ponuda.in.rs",
+      "https://specijalna-ponuda.in.rs",
     ],
     credentials: true,
   })
