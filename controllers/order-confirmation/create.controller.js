04531fb32258adbad6fd27b664b826dd3c8415e5
Dejan Markovic <dejan.markovic@cinnamon.agency>
2022-11-23 15:40:36 +0100

    update:

--- controllers/order-confirmation/create.controller.js
+++ controllers/order-confirmation/create.controller.js
@@ -5,22 +5,28 @@ const nodemailer = require("nodemailer");
 exports.create = tryCatch(async (req, res) => {
   const { email, id, model, quantity } = req.body;
 
+  console.log("test process.env.USER", process.env.USER);
+
   const mailTransporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: "specijalna.ponuda.in.rs@gmail.com",
-      pass: "fgfgqoqdudyoiumc",
+      user: process.env.USER,
+      pass: process.env.PASS,
     },
   });
 
   const details = {
-    from: "specijalna.ponuda.in.rs@gmail.com",
+    // from: "specijalna.ponuda.in.rs@gmail.com",
+    from: process.env.USER,
     to: `${email}`,
     subject: "Verifikacioni email - Specijalna ponuda",
     html: `<div> <p>Poštovani, u nastavku se nalazi link za potvrdu rezervacije ${model} ${quantity} uređaja.</p></br><p>Link za potvrdu rezervacije: http://localhost:1234/order-confirm.html?${id}</p></div>`,
   };
 
   mailTransporter.sendMail(details, (err) => {
+    console.log("test details", details);
+    console.log("test mailTransporter", mailTransporter);
+
     if (err) {
       return console.log("test err", err);
     }
